5631a96d73b1515bf2a13d9816abee5b60b7f61b
Michael Westbom <totallymike@gmail.com>
2012-12-27 12:42:49 -0500

    Lots of crap.  Went too long before committing.

--- lib/NNTPClient.rb
+++ lib/NNTPClient.rb
@@ -1,5 +1,70 @@
+require 'socket'
 require "NNTPClient/version"
 
-module NNTPClient
-  # Your code goes here...
+class NNTPClient
+  attr_reader :socket, :status, :current_group,
+              :groups
+
+  def initialize(url, port=119)
+    @socket = TCPSocket.new(url, port)
+    @current_group = nil
+    @status = nil
+    @groups = nil
+  end
+
+  def list_groups
+    send_message "LIST"
+    status = get_status
+    return nil unless status[:code] == 215
+
+    self.groups = get_data_block
+  end
+
+  def group(group)
+    send_message "GROUP #{group}"
+    self.status = get_status
+    if status[:code] == 211
+      self.current_group = status[:params][-1]
+    end
+  end
+
+  private
+  def groups=(list=[])
+    @groups = list
+  end
+
+  def status=(status)
+    @status = status
+  end
+
+  def current_group=(group)
+    @current_group = group
+  end
+
+  def get_status
+    line = get_line
+    {
+      :code => line[0..2].to_i,
+      :message => line[3..-1].lstrip,
+      :params => line.split
+    }
+  end
+
+  def get_data_block
+    lines = []
+    current_line = get_line
+    until current_line == '.'
+      lines << current_line
+      current_line = get_line
+    end
+    lines
+  end
+
+  def get_line
+    socket.gets().chomp
+  end
+
+  def send_message(msg)
+    socket.print "#{msg}\r\n"
+  end
 end
--- spec/NNTPClient_spec.rb
+++ spec/NNTPClient_spec.rb
@@ -1,9 +1,49 @@
+require_relative 'spec_helper'
+require 'socket'
+
 describe NNTPClient do
+  before (:each) do
+    @sock = double()
+  end
+
+  let (:nntp) do
+    TCPSocket.stub(:new).and_return @sock
+    NNTPClient.new('nntp.example.com', 119)
+  end
+
   it 'should have a version number' do
     NNTPClient::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe '#groups' do
+
+    let (:groups) do
+      ["215 list of newsgroups follows\r\n", "alt.bin.foo\r\n",
+       "alt.bin.bar\r\n", ".\r\n"]
+    end
+
+    before(:each) do
+      @sock.should_receive(:print).with("LIST\r\n")
+      @sock.should_receive(:gets).and_return(groups[0], groups[1], groups[2], groups[3])
+    end
+    it 'can request a list of groups' do
+      groups_response = nntp.list_groups
+      groups_response.should eq %w(alt.bin.foo alt.bin.bar)
+      nntp.groups.should eq %w(alt.bin.foo alt.bin.bar)
+    end
+  end
+
+  describe '#group' do
+    let (:group) do 'alt.bin.foo.bar' end
+
+    before(:each) do
+      @sock.should_receive(:print).with("GROUP #{group}\r\n")
+      @sock.should_receive(:gets).and_return("211 2 1 2 alt.bin.foo.bar\r\n")
+    end
+    it 'can select a group from the server' do
+      nntp.group group
+      nntp.status[:code].should eq 211
+      nntp.current_group.should eq group
+    end
   end
 end
