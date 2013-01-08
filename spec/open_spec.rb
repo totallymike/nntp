9670baa07c89c8c93f5e8d103b944741873fa073
Michael Westbom <totallymike@gmail.com>
2013-01-08 11:36:08 -0500

    Changed test for to more accurately display intent.

--- spec/open_spec.rb
+++ spec/open_spec.rb
@@ -9,7 +9,8 @@ describe "NNTP" do
       conn = NNTP.open(:socket => sock)
       conn.should_not be nil
     end
-    it "can also accept a url and a port" do
+    it "will open its own socket if given a url and a port number" do
+      TCPSocket.should_receive :new
       conn = NNTP.open(:url => 'nntp.example.org', :port => 119)
       conn.should_not be nil
     end
