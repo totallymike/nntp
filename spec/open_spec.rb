651952d5c57d10a0a1e89c1b14c67fefbfa8121e
Michael Westbom <totallymike@gmail.com>
2013-01-09 10:38:53 -0500

    Added a test, simplified another.

--- spec/open_spec.rb
+++ spec/open_spec.rb
@@ -10,14 +10,18 @@ describe "NNTP" do
       conn.should_not be nil
     end
     it "will open its own socket if given a url and a port number" do
-      TCPSocket.should_receive :new
+      TCPSocket.should_receive(:new).with("nntp.example.org", 119)
       conn = NNTP.open(:url => 'nntp.example.org', :port => 119)
       conn.should_not be nil
+    end
+    it "uses port number 119 as a default if no port is specified" do
+      TCPSocket.should_receive(:new).with("nntp.example.org", 119)
+      NNTP.open(:url => 'nntp.example.org')
     end
     it "can take a class as :socket_factory alongside the url and port" do
       foo = double()
       foo.should_receive(:new).with('nntp.example.org', 119)
-      NNTP.open(:url => 'nntp.example.org', :port => 119, :socket_factory => foo)
+      NNTP.open(:url => 'nntp.example.org', :socket_factory => foo)
     end
   end
 end
