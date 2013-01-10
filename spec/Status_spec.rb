5604a9a88532314ef2d8d9946afb39c91a12d627
Michael Westbom <totallymike@gmail.com>
2013-01-10 15:22:32 -0500

    Made spec a little more descriptive.

--- spec/Status_spec.rb
+++ spec/Status_spec.rb
@@ -2,8 +2,8 @@ require_relative "spec_helper"
 require "nntp/status"
 
 describe NNTP::Status do
-  describe "to_s" do
-    it "should presents full message with to_s" do
+  describe "#to_s" do
+    it "knows how to render the server response it represents" do
       foo = NNTP::Status.new(100, "foo")
       foo.to_s.should eq "100 foo"
       bar = NNTP::Status.new(200, "bar")
